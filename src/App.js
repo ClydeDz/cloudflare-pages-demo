c3e302e2de44ff99c02f7b478084d82ff9f984b9
clydedz <clyd94@gmail.com>
2021-03-05 00:01:48 +1300

    Update 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Update # 1
+          Update # 2
         </p>
         <a
           className="App-link"
