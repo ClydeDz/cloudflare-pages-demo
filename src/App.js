614c0a8896e013a26938bb70c0046de353e4a9ea
clydedz <clyd94@gmail.com>
2021-03-05 00:15:41 +1300

    Minor update 3.1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Update # 3
+          Update # 3.1
         </p>
         <a
           className="App-link"
